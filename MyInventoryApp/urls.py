0043ec9a99c7283a8c848b2b619ce5b240f9a061
jim-aban <jim.aban@student.ateneo.edu>
2025-04-10 09:58:24 +0800

    Update urls.py

--- MyInventoryApp/urls.py
+++ MyInventoryApp/urls.py
@@ -1,20 +1,3 @@
-"""
-URL configuration for MyInventorySystem project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.urls import path
 from . import views
 
@@ -23,9 +6,11 @@ urlpatterns = [
     path('signup/', views.signup_view, name='signup'),
     path('view_supplier/', views.view_supplier, name='view_supplier'),
     path('view_bottles/', views.view_bottles, name='view_bottles'),
+    path('view_bottle_details/<int:pk>/', views.view_bottle_details, name='view_bottle_details'),
+    path('delete_bottle/<int:pk2>/', views.delete_bottle, name='delete_bottle'),
     path('add_bottle/', views.add_bottle, name='add_bottle'),
-    path('logout/', views.logout_view, name='logout'),
     path('manage_account/<int:pk>/', views.manage_account, name='manage_account'),
     path('delete_account/<int:pk>/', views.delete_account, name='delete_account'),
     path('change_password/<int:pk>/', views.change_password, name='change_password'),
+    path('logout/', views.logout_view, name='logout'),
 ]
